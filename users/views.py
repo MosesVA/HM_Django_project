c5f50a2c4042aae0dbc45b81c5b48134b1b7e96d
Vladislav Moiseev <morninyep@gmail.com>
2024-10-22 00:44:26 +0500

    Добавлена FBV user_register_view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from users.models import User
+from users.forms import UserRegisterForm
 
-# Create your views here.
+
+def user_register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponseRedirect(reverse('dogs:index'))
+    return render(request, 'users/register_user.html', {'form': UserRegisterForm})
