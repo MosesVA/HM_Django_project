d41cea971c6039a8397a172f3ede4bfdfc367052
Vladislav Moiseev <morninyep@gmail.com>
2024-10-23 13:57:51 +0500

    Добавлена возможность редактирования профиля.

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, reverse, redirect
-from users.models import User
-from users.forms import UserRegisterForm, UserLoginForm, UserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm
 
 
 def user_register_view(request):
@@ -38,11 +38,27 @@ def user_login_view(request):
 def user_profile_view(request):
     user_object = request.user
     context = {
-        # 'user_object': user_object,
-        'title': f'Ваш профиль, {user_object.first_name}',
-        # 'form': UserForm(instance=user_object)
+        'title': f'Ваш профиль {user_object.first_name}',
     }
     return render(request, 'users/user_profile_read_only.html', context)
+
+
+@login_required
+def user_update_view(request):
+    user_object = request.user
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, request.FILES, instance=user_object)
+        if form.is_valid():
+            user_object = form.save()
+            user_object.save()
+            return HttpResponseRedirect(reverse('users:profile_user'))
+    user_name = user_object.first_name
+    context = {
+        'object': user_object,
+        'title': f'Изменить профиль {user_name}',
+        'form': UserUpdateForm(instance=user_object)
+    }
+    return render(request, 'users/update_user.html', context)
 
 
 def user_logout_view(request):
