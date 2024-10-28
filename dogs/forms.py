812ce77b99a5b61d8fb4f33cd4f19c97fde14f35
Vladislav Moiseev <morninyep@gmail.com>
2024-10-29 01:41:24 +0500

    Обновлена форма DogForm, добавлена валидация даты рождения

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django import forms
 
 from dogs.models import Dog
@@ -7,4 +9,11 @@ from users.forms import StyleFormMixin
 class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
-        fields = '__all__'
+        exclude = ('owner',)
+
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_date']
+        now_year = datetime.now().year
+        if now_year - cleaned_data.year > 100:
+            raise forms.ValidationError('Собака не может быть старше 100 лет')
+        return cleaned_data
